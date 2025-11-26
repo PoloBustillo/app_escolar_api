0b484b959f010b6d571af1bb0fbddf52d4b75400
PoloBustillo <39663418+PoloBustillo@users.noreply.github.com>
2025-11-25 19:31:37 -0600

    ALLOWED HOST

--- app_escolar_api/settings.py
+++ app_escolar_api/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = '-_&+lsebec(whhw!%n@ww&1j=4-^j_if9x8$q778+99oz&!ms2'
 
 DEBUG = True  # en desarrollo
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1","appescolarapi-production-56e4.up.railway.app",]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1","appescolarapi-production-56e4.up.railway.app","appescolarapi-production-b5ec.up.railway.app"]
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
